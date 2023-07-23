74e1e77fa6238bfa471634be62cd0c3ac759f08e
Roman <vikulov.roman.work@gmail.com>
2023-07-23 16:58:52 +0300

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,10 +3,15 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    openModalBtn2: document.querySelector('[data-modal-open2]'),
+    closeModalBtn2: document.querySelector('[data-modal-close2]'),
+    modal2: document.querySelector('[data-modal2]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn2.addEventListener('click', toggleModal);
+  refs.closeModalBtn2.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
