80afae20bda6f7d10eaac1beaa8f7688393b1249
Roman <vikulov.roman.work@gmail.com>
2023-09-01 11:08:41 +0300

    Update tg.js

--- src/js/tg.js
+++ src/js/tg.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const { TOKEN } = process.env.TOKEN;
 
 const CHAT_ID = `-1001933429777`;
-const URL_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+const URL_API = `https://api.telegram.org/bot${process.env.TOKEN}/sendMessage`;
 const success = document.getElementById('success');
 
 document.getElementById('tg').addEventListener('submit', function (e) {
