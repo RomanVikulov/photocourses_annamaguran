1567c082c19bcc8a3a4e14cafa899a4dc18299f5
RomanVikulov <103632396+RomanVikulov@users.noreply.github.com>
2023-09-14 07:07:50 +0000

    Deploying to gh-pages from @ RomanVikulov/photocourses_annamaguran@67bd7f048fb4ae57b2e7aa43dd6a1e2113458a7a 🚀

--- index.0323d576.js
+++
@@ -1,3 +0,0 @@
-const e=document.getElementById("success");document.getElementById("tg-form").addEventListener("submit",function(t){t.preventDefault();let n=this.name.value,l=this.tel.value,s=this.comment.value;axios.post("/send-message",{name:n,tel:l,comment:s}).then(t=>{console.log("Статус ответа:",t.status),console.log("Данные ответа:",t.data),this.name.value="",this.tel.value="",this.comment.value="",e.innerHTML="Сообщение отправлено!",e.style.display="block"}).catch(e=>{console.warn(e)}).finally(()=>{console.log("Отправили!")})});//# sourceMappingURL=index.0323d576.js.map
-
-//# sourceMappingURL=index.0323d576.js.map
